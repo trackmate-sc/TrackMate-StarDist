cdbb698063f4852dead7a61a8694c7c33596b4f9
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2020-04-22 16:33:08 +0200

    StarDist detector factory, config panel and logo.
    For proper integration into TrackMate.

---
+++ src/main/java/fiji/plugin/trackmate/stardist/StarDistDetectorConfigurationPanel.java
@@ -0,0 +1,341 @@
+package fiji.plugin.trackmate.stardist;
+
+import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_TARGET_CHANNEL;
+import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_THRESHOLD;
+import static fiji.plugin.trackmate.gui.TrackMateWizard.BIG_FONT;
+import static fiji.plugin.trackmate.gui.TrackMateWizard.FONT;
+import static fiji.plugin.trackmate.gui.TrackMateWizard.SMALL_FONT;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import fiji.plugin.trackmate.Logger;
+import fiji.plugin.trackmate.Model;
+import fiji.plugin.trackmate.Settings;
+import fiji.plugin.trackmate.Spot;
+import fiji.plugin.trackmate.SpotCollection;
+import fiji.plugin.trackmate.TrackMate;
+import fiji.plugin.trackmate.detection.SpotDetectorFactory;
+import fiji.plugin.trackmate.gui.ConfigurationPanel;
+import fiji.plugin.trackmate.gui.TrackMateGUIController;
+import fiji.plugin.trackmate.util.JLabelLogger;
+import ij.ImagePlus;
+
+public class StarDistDetectorConfigurationPanel extends ConfigurationPanel
+{
+
+	private static final long serialVersionUID = 1L;
+
+	private static final String TITLE = "StarDist detector";
+
+	protected static final ImageIcon ICON = new ImageIcon( getResource( "images/TrackMateStarDist-logo100x100.png" ) );
+
+	private static final ImageIcon ICON_PREVIEW = new ImageIcon( TrackMateGUIController.class.getResource( "images/flag_checked.png" ) );
+
+	private static final NumberFormat THRESHOLD_FORMAT = new DecimalFormat( "#.##" );
+
+	private final JSlider sliderChannel;
+
+	private final JFormattedTextField ftfThreshold;
+
+	private final JButton btnPreview;
+
+	private final Logger localLogger;
+
+	private final Model model;
+
+	private final ImagePlus imp;
+
+	/**
+	 * Create the panel.
+	 */
+	public StarDistDetectorConfigurationPanel( final Settings settings, final Model model )
+	{
+		this.model = model;
+		this.imp = settings.imp;
+
+		final GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 200, 0, 32 };
+		gridBagLayout.rowHeights = new int[] { 0, 0, 145, 0, 0, 55, 23 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 1.0, 0.0 };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+		setLayout( gridBagLayout );
+
+		final JLabel lblSettingsForDetector = new JLabel( "Settings for detector:" );
+		lblSettingsForDetector.setFont( FONT );
+		final GridBagConstraints gbc_lblSettingsForDetector = new GridBagConstraints();
+		gbc_lblSettingsForDetector.gridwidth = 3;
+		gbc_lblSettingsForDetector.insets = new Insets( 5, 5, 5, 5 );
+		gbc_lblSettingsForDetector.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblSettingsForDetector.gridx = 0;
+		gbc_lblSettingsForDetector.gridy = 0;
+		add( lblSettingsForDetector, gbc_lblSettingsForDetector );
+
+		final JLabel lblStardistDetector = new JLabel( TITLE, ICON, JLabel.RIGHT );
+		lblStardistDetector.setFont( BIG_FONT );
+		lblStardistDetector.setHorizontalAlignment( SwingConstants.CENTER );
+		final GridBagConstraints gbc_lblStardistDetector = new GridBagConstraints();
+		gbc_lblStardistDetector.gridwidth = 3;
+		gbc_lblStardistDetector.insets = new Insets( 5, 5, 5, 5 );
+		gbc_lblStardistDetector.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblStardistDetector.gridx = 0;
+		gbc_lblStardistDetector.gridy = 1;
+		add( lblStardistDetector, gbc_lblStardistDetector );
+
+		/*
+		 * Help text.
+		 */
+		final JLabel lblHelptext = new JLabel( StarDistDetectorFactory.INFO_TEXT
+				.replace( "<br>", "" )
+				.replace( "<p>", "<p align=\"justify\">" )
+				.replace( "<html>", "<html><p align=\"justify\">" ) );
+		lblHelptext.setFont( FONT.deriveFont( Font.ITALIC ) );
+		final GridBagConstraints gbc_lblHelptext = new GridBagConstraints();
+		gbc_lblHelptext.anchor = GridBagConstraints.NORTH;
+		gbc_lblHelptext.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblHelptext.gridwidth = 3;
+		gbc_lblHelptext.insets = new Insets( 5, 5, 5, 5 );
+		gbc_lblHelptext.gridx = 0;
+		gbc_lblHelptext.gridy = 2;
+		add( lblHelptext, gbc_lblHelptext );
+
+		/*
+		 * Channel selector.
+		 */
+
+
+		final JLabel lblSegmentInChannel = new JLabel( "Segment in channel:" );
+		lblSegmentInChannel.setFont( SMALL_FONT );
+		final GridBagConstraints gbc_lblSegmentInChannel = new GridBagConstraints();
+		gbc_lblSegmentInChannel.anchor = GridBagConstraints.EAST;
+		gbc_lblSegmentInChannel.insets = new Insets( 5, 5, 5, 5 );
+		gbc_lblSegmentInChannel.gridx = 0;
+		gbc_lblSegmentInChannel.gridy = 3;
+		add( lblSegmentInChannel, gbc_lblSegmentInChannel );
+
+		sliderChannel = new JSlider();
+		final JLabel labelChannel = new JLabel( "1" );
+		sliderChannel.addChangeListener( new ChangeListener()
+		{
+			@Override
+			public void stateChanged( final ChangeEvent e )
+			{
+				labelChannel.setText( "" + sliderChannel.getValue() );
+			}
+		} );
+		final GridBagConstraints gbc_sliderChannel = new GridBagConstraints();
+		gbc_sliderChannel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_sliderChannel.insets = new Insets( 5, 5, 5, 5 );
+		gbc_sliderChannel.gridx = 1;
+		gbc_sliderChannel.gridy = 3;
+		add( sliderChannel, gbc_sliderChannel );
+
+		labelChannel.setHorizontalAlignment( SwingConstants.CENTER );
+		labelChannel.setFont( SMALL_FONT );
+		final GridBagConstraints gbc_labelChannel = new GridBagConstraints();
+		gbc_labelChannel.insets = new Insets( 5, 5, 5, 5 );
+		gbc_labelChannel.gridx = 2;
+		gbc_labelChannel.gridy = 3;
+		add( labelChannel, gbc_labelChannel );
+
+		/*
+		 * Threshold.
+		 */
+
+		final JLabel lblThreshold = new JLabel( "Threshold:" );
+		lblThreshold.setFont( SMALL_FONT );
+		final GridBagConstraints gbc_lblThreshold = new GridBagConstraints();
+		gbc_lblThreshold.anchor = GridBagConstraints.EAST;
+		gbc_lblThreshold.insets = new Insets( 5, 5, 5, 5 );
+		gbc_lblThreshold.gridx = 0;
+		gbc_lblThreshold.gridy = 4;
+		add( lblThreshold, gbc_lblThreshold );
+
+		ftfThreshold = new JFormattedTextField( THRESHOLD_FORMAT );
+		ftfThreshold.setMinimumSize( new Dimension( 60, 26 ) );
+		ftfThreshold.setHorizontalAlignment( SwingConstants.CENTER );
+		ftfThreshold.setFont( FONT );
+		final GridBagConstraints gbc_ftfThreshold = new GridBagConstraints();
+		gbc_ftfThreshold.gridwidth = 2;
+		gbc_ftfThreshold.insets = new Insets( 5, 5, 5, 5 );
+		gbc_ftfThreshold.gridx = 1;
+		gbc_ftfThreshold.gridy = 4;
+		add( ftfThreshold, gbc_ftfThreshold );
+
+		/*
+		 * Preview.
+		 */
+
+		btnPreview = new JButton( "Preview", ICON_PREVIEW );
+		btnPreview.setFont( FONT );
+		final GridBagConstraints gbc_btnPreview = new GridBagConstraints();
+		gbc_btnPreview.gridwidth = 2;
+		gbc_btnPreview.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnPreview.anchor = GridBagConstraints.SOUTH;
+		gbc_btnPreview.insets = new Insets( 5, 5, 5, 5 );
+		gbc_btnPreview.gridx = 1;
+		gbc_btnPreview.gridy = 5;
+		add( btnPreview, gbc_btnPreview );
+
+		/*
+		 * Logger.
+		 */
+
+		final JLabelLogger labelLogger = new JLabelLogger();
+		final GridBagConstraints gbc_labelLogger = new GridBagConstraints();
+		gbc_labelLogger.anchor = GridBagConstraints.EAST;
+		gbc_labelLogger.gridwidth = 3;
+		gbc_labelLogger.insets = new Insets( 0, 0, 0, 5 );
+		gbc_labelLogger.gridx = 0;
+		gbc_labelLogger.gridy = 6;
+		add( labelLogger, gbc_labelLogger );
+		localLogger = labelLogger.getLogger();
+
+		/*
+		 * Listeners and specificities.
+		 */
+
+		/*
+		 * Deal with channels: the slider and channel labels are only visible if
+		 * we find more than one channel.
+		 */
+		if ( null != imp )
+		{
+			final int n_channels = imp.getNChannels();
+			sliderChannel.setMaximum( n_channels );
+			sliderChannel.setMinimum( 1 );
+			sliderChannel.setValue( imp.getChannel() );
+
+			if ( n_channels <= 1 )
+			{
+				labelChannel.setVisible( false );
+				lblSegmentInChannel.setVisible( false );
+				sliderChannel.setVisible( false );
+			}
+			else
+			{
+				labelChannel.setVisible( true );
+				lblSegmentInChannel.setVisible( true );
+				sliderChannel.setVisible( true );
+			}
+		}
+
+		btnPreview.addActionListener( new ActionListener()
+		{
+			@Override
+			public void actionPerformed( final ActionEvent e )
+			{
+				preview();
+			}
+		} );
+	}
+
+	@Override
+	public Map< String, Object > getSettings()
+	{
+		final HashMap< String, Object > settings = new HashMap<>( 2 );
+		final int targetChannel = sliderChannel.getValue();
+		final double threshold = ( ( Number ) ftfThreshold.getValue() ).doubleValue();
+		settings.put( KEY_TARGET_CHANNEL, targetChannel );
+		settings.put( KEY_THRESHOLD, threshold );
+		return settings;
+	}
+
+	@Override
+	public void setSettings( final Map< String, Object > settings )
+	{
+		sliderChannel.setValue( ( Integer ) settings.get( KEY_TARGET_CHANNEL ) );
+		ftfThreshold.setValue( settings.get( KEY_THRESHOLD ) );
+	}
+
+	@Override
+	public void clean()
+	{}
+
+	@SuppressWarnings( "rawtypes" )
+	protected SpotDetectorFactory< ? > getDetectorFactory()
+	{
+		return new StarDistDetectorFactory();
+	}
+
+	/**
+	 * Launch detection on the current frame.
+	 */
+	protected void preview()
+	{
+		btnPreview.setEnabled( false );
+		new Thread( "TrackMate preview detection thread" )
+		{
+			@Override
+			public void run()
+			{
+				final Settings settings = new Settings();
+				settings.setFrom( imp );
+				final int frame = imp.getFrame() - 1;
+				settings.tstart = frame;
+				settings.tend = frame;
+
+				settings.detectorFactory = getDetectorFactory();
+				settings.detectorSettings = getSettings();
+
+				final TrackMate trackmate = new TrackMate( settings );
+				trackmate.getModel().setLogger( localLogger );
+
+				final boolean detectionOk = trackmate.execDetection();
+				if ( !detectionOk )
+				{
+					localLogger.error( trackmate.getErrorMessage() );
+					return;
+				}
+				localLogger.log( "Found " + trackmate.getModel().getSpots().getNSpots( false ) + " spots." );
+
+				// Wrap new spots in a list.
+				final SpotCollection newspots = trackmate.getModel().getSpots();
+				final Iterator< Spot > it = newspots.iterator( frame, false );
+				final ArrayList< Spot > spotsToCopy = new ArrayList<>( newspots.getNSpots( frame, false ) );
+				while ( it.hasNext() )
+				{
+					spotsToCopy.add( it.next() );
+				}
+				// Pass new spot list to model.
+				model.getSpots().put( frame, spotsToCopy );
+				// Make them visible
+				for ( final Spot spot : spotsToCopy )
+				{
+					spot.putFeature( SpotCollection.VISIBLITY, SpotCollection.ONE );
+				}
+				// Generate event for listener to reflect changes.
+				model.setSpots( model.getSpots(), true );
+
+				btnPreview.setEnabled( true );
+
+			}
+		}.start();
+	}
+
+	protected static URL getResource( final String name )
+	{
+		return StarDistDetectorFactory.class.getClassLoader().getResource( name );
+	}
+}
---
+++ src/main/java/fiji/plugin/trackmate/stardist/StarDistDetectorFactory.java
@@ -0,0 +1,234 @@
+package fiji.plugin.trackmate.stardist;
+
+import static fiji.plugin.trackmate.detection.DetectorKeys.DEFAULT_TARGET_CHANNEL;
+import static fiji.plugin.trackmate.detection.DetectorKeys.DEFAULT_THRESHOLD;
+import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_TARGET_CHANNEL;
+import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_THRESHOLD;
+import static fiji.plugin.trackmate.io.IOUtils.readDoubleAttribute;
+import static fiji.plugin.trackmate.io.IOUtils.readIntegerAttribute;
+import static fiji.plugin.trackmate.io.IOUtils.writeTargetChannel;
+import static fiji.plugin.trackmate.io.IOUtils.writeThreshold;
+import static fiji.plugin.trackmate.util.TMUtils.checkMapKeys;
+import static fiji.plugin.trackmate.util.TMUtils.checkParameter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.ImageIcon;
+
+import org.jdom2.Element;
+import org.scijava.plugin.Plugin;
+
+import fiji.plugin.trackmate.Model;
+import fiji.plugin.trackmate.Settings;
+import fiji.plugin.trackmate.detection.SpotDetector;
+import fiji.plugin.trackmate.detection.SpotDetectorFactory;
+import fiji.plugin.trackmate.gui.ConfigurationPanel;
+import fiji.plugin.trackmate.util.TMUtils;
+import net.imagej.ImgPlus;
+import net.imglib2.Interval;
+import net.imglib2.RandomAccessible;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.view.Views;
+
+@Plugin( type = SpotDetectorFactory.class )
+public class StarDistDetectorFactory< T extends RealType< T > & NativeType< T > > implements SpotDetectorFactory< T >
+{
+
+	/*
+	 * CONSTANTS
+	 */
+
+	/** A string key identifying this factory. */
+	public static final String DETECTOR_KEY = "STARDIST_DETECTOR";
+
+	/** The pretty name of the target detector. */
+	public static final String NAME = "StarDist detector";
+
+	/** An html information text. */
+	public static final String INFO_TEXT = "<html>"
+			+ "This detector relies on StarDist to detect cells."
+			+ "<p>"
+			+ "It only works for 2D images."
+			+ "And for this detector to work, the update sites StartDist and CSBDeep "
+			+ "must be activated in your Fiji installation."
+			+ "<p>"
+			+ "The StarDist versatile model for fluorescencent cells is used, "
+			+ "along with the default parameters. Spots are created from the cell "
+			+ "segmentation results, with a radius set from the cell area, and a "
+			+ "quality equal to the maximal value of the probability image in the cell. "
+			+ "<p>"
+			+ "The treshold on quality corresponds to a probability value, ranging from 0 to 1."
+			+ "</html>";
+
+	/*
+	 * FIELDS
+	 */
+
+	/** The image to operate on. Multiple frames, single channel. */
+	protected ImgPlus< T > img;
+
+	protected Map< String, Object > settings;
+
+	protected String errorMessage;
+
+	/*
+	 * METHODS
+	 */
+
+	@Override
+	public SpotDetector< T > getDetector( final Interval interval, final int frame )
+	{
+		final double threshold = ( Double ) settings.get( KEY_THRESHOLD );
+		final double[] calibration = TMUtils.getSpatialCalibration( img );
+		final RandomAccessible< T > imFrame = prepareFrameImg( frame );
+		final StarDistDetector< T > detector = new StarDistDetector< >( imFrame, interval, calibration, threshold );
+		return detector;
+	}
+
+	@Override
+	public boolean setTarget( final ImgPlus< T > img, final Map< String, Object > settings )
+	{
+		this.img = img;
+		this.settings = settings;
+		return checkSettings( settings );
+	}
+
+
+	@Override
+	public String getErrorMessage()
+	{
+		return errorMessage;
+	}
+
+	@Override
+	public boolean marshall( final Map< String, Object > settings, final Element element )
+	{
+		final StringBuilder errorHolder = new StringBuilder();
+		final boolean ok = writeTargetChannel( settings, element, errorHolder )
+				&& writeThreshold( settings, element, errorHolder );
+
+		if ( !ok )
+			errorMessage = errorHolder.toString();
+
+		return ok;
+	}
+
+	@Override
+	public boolean unmarshall( final Element element, final Map< String, Object > settings )
+	{
+		settings.clear();
+		final StringBuilder errorHolder = new StringBuilder();
+		boolean ok = true;
+		ok = ok & readDoubleAttribute( element, settings, KEY_THRESHOLD, errorHolder );
+		ok = ok & readIntegerAttribute( element, settings, KEY_TARGET_CHANNEL, errorHolder );
+		if ( !ok )
+		{
+			errorMessage = errorHolder.toString();
+			return false;
+		}
+		return checkSettings( settings );
+	}
+
+	@Override
+	public ConfigurationPanel getDetectorConfigurationPanel( final Settings settings, final Model model )
+	{
+		return new StarDistDetectorConfigurationPanel( settings, model );
+	}
+
+	@Override
+	public Map< String, Object > getDefaultSettings()
+	{
+		final Map< String, Object > lSettings = new HashMap<>();
+		lSettings.put( KEY_TARGET_CHANNEL, DEFAULT_TARGET_CHANNEL );
+		lSettings.put( KEY_THRESHOLD, DEFAULT_THRESHOLD );
+		return lSettings;
+	}
+
+	@Override
+	public boolean checkSettings( final Map< String, Object > lSettings )
+	{
+		boolean ok = true;
+		final StringBuilder errorHolder = new StringBuilder();
+		ok = ok & checkParameter( lSettings, KEY_TARGET_CHANNEL, Integer.class, errorHolder );
+		ok = ok & checkParameter( lSettings, KEY_THRESHOLD, Double.class, errorHolder );
+		final List< String > mandatoryKeys = new ArrayList<>();
+		mandatoryKeys.add( KEY_TARGET_CHANNEL );
+		mandatoryKeys.add( KEY_THRESHOLD );
+		ok = ok & checkMapKeys( lSettings, mandatoryKeys, null, errorHolder );
+		if ( !ok )
+			errorMessage = errorHolder.toString();
+
+		return ok;
+	}
+
+
+	@Override
+	public String getInfoText()
+	{
+		return INFO_TEXT;
+	}
+
+	@Override
+	public ImageIcon getIcon()
+	{
+		return null;
+	}
+
+	@Override
+	public String getKey()
+	{
+		return DETECTOR_KEY;
+	}
+
+	@Override
+	public String getName()
+	{
+		return NAME;
+	}
+
+	protected RandomAccessible< T > prepareFrameImg( final int frame )
+	{
+		final double[] calibration = TMUtils.getSpatialCalibration( img );
+		RandomAccessible< T > imFrame;
+		final int cDim = TMUtils.findCAxisIndex( img );
+		if ( cDim < 0 )
+		{
+			imFrame = img;
+		}
+		else
+		{
+			// In ImgLib2, dimensions are 0-based.
+			final int channel = ( Integer ) settings.get( KEY_TARGET_CHANNEL ) - 1;
+			imFrame = Views.hyperSlice( img, cDim, channel );
+		}
+
+		int timeDim = TMUtils.findTAxisIndex( img );
+		if ( timeDim >= 0 )
+		{
+			if ( cDim >= 0 && timeDim > cDim )
+			{
+				timeDim--;
+			}
+			imFrame = Views.hyperSlice( imFrame, timeDim, frame );
+		}
+
+		// In case we have a 1D image.
+		if ( img.dimension( 0 ) < 2 )
+		{ // Single column image, will be rotated internally.
+			calibration[ 0 ] = calibration[ 1 ]; // It gets NaN otherwise
+			calibration[ 1 ] = 1;
+			imFrame = Views.hyperSlice( imFrame, 0, 0 );
+		}
+		if ( img.dimension( 1 ) < 2 )
+		{ // Single line image
+			imFrame = Views.hyperSlice( imFrame, 1, 0 );
+		}
+
+		return imFrame;
+	}
+
+}
