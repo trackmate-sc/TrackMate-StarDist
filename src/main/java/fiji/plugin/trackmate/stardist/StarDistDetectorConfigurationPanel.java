e7137c556c2e497ea9767cd53882914b552f3bd1
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2020-04-27 01:37:23 +0200

    Make the stardist detector config panel more extensible.

--- src/main/java/fiji/plugin/trackmate/stardist/StarDistDetectorConfigurationPanel.java
+++ src/main/java/fiji/plugin/trackmate/stardist/StarDistDetectorConfigurationPanel.java
@@ -9,15 +9,9 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JSlider;
@@ -28,24 +22,15 @@ import javax.swing.event.ChangeListener;
 import fiji.plugin.trackmate.Logger;
 import fiji.plugin.trackmate.Model;
 import fiji.plugin.trackmate.Settings;
-import fiji.plugin.trackmate.Spot;
-import fiji.plugin.trackmate.SpotCollection;
-import fiji.plugin.trackmate.TrackMate;
 import fiji.plugin.trackmate.detection.SpotDetectorFactory;
-import fiji.plugin.trackmate.gui.ConfigurationPanel;
-import fiji.plugin.trackmate.gui.TrackMateGUIController;
 import fiji.plugin.trackmate.util.JLabelLogger;
 
-public class StarDistDetectorConfigurationPanel extends ConfigurationPanel
+public class StarDistDetectorConfigurationPanel extends StarDistDetectorBaseConfigurationPanel
 {
 
 	private static final long serialVersionUID = 1L;
 
 	private static final String TITLE = "StarDist detector";
-
-	protected static final ImageIcon ICON = new ImageIcon( getResource( "images/TrackMateStarDist-logo100x100.png" ) );
-
-	private static final ImageIcon ICON_PREVIEW = new ImageIcon( TrackMateGUIController.class.getResource( "images/flag_checked.png" ) );
 
 	private final JSlider sliderChannel;
 
@@ -53,17 +38,9 @@ public class StarDistDetectorConfigurationPanel extends ConfigurationPanel
 
 	private final Logger localLogger;
 
-	private final Model model;
-
-	private final Settings settings;
-
-	/**
-	 * Create the panel.
-	 */
 	public StarDistDetectorConfigurationPanel( final Settings settings, final Model model )
 	{
-		this.settings = settings;
-		this.model = model;
+		super( settings, model );
 
 		final GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[] { 200, 0, 32 };
@@ -207,14 +184,7 @@ public class StarDistDetectorConfigurationPanel extends ConfigurationPanel
 			}
 		}
 
-		btnPreview.addActionListener( new ActionListener()
-		{
-			@Override
-			public void actionPerformed( final ActionEvent e )
-			{
-				preview();
-			}
-		} );
+		btnPreview.addActionListener( l -> preview( btnPreview, localLogger ) );
 	}
 
 	@Override
@@ -236,74 +206,10 @@ public class StarDistDetectorConfigurationPanel extends ConfigurationPanel
 	public void clean()
 	{}
 
+	@Override
 	@SuppressWarnings( "rawtypes" )
 	protected SpotDetectorFactory< ? > getDetectorFactory()
 	{
 		return new StarDistDetectorFactory();
-	}
-
-	/**
-	 * Launch detection on the current frame.
-	 */
-	protected void preview()
-	{
-		btnPreview.setEnabled( false );
-		new Thread( "TrackMate preview detection thread" )
-		{
-			@Override
-			public void run()
-			{
-				final Settings lSettings = new Settings();
-				lSettings.setFrom( settings.imp );
-				final int frame = settings.imp.getFrame() - 1;
-				lSettings.tstart = frame;
-				lSettings.tend = frame;
-				lSettings.roi = settings.roi;
-				lSettings.xstart = settings.xstart;
-				lSettings.xend = settings.xend;
-				lSettings.ystart = settings.ystart;
-				lSettings.yend = settings.yend;
-				lSettings.zstart = settings.zstart;
-				lSettings.zend = settings.zend;
-
-				lSettings.detectorFactory = getDetectorFactory();
-				lSettings.detectorSettings = getSettings();
-
-				final TrackMate trackmate = new TrackMate( lSettings );
-				trackmate.getModel().setLogger( localLogger );
-
-				final boolean detectionOk = trackmate.execDetection();
-				if ( !detectionOk )
-				{
-					localLogger.error( trackmate.getErrorMessage() );
-					return;
-				}
-				localLogger.log( "Found " + trackmate.getModel().getSpots().getNSpots( false ) + " spots." );
-
-				// Wrap new spots in a list.
-				final SpotCollection newspots = trackmate.getModel().getSpots();
-				final Iterator< Spot > it = newspots.iterator( frame, false );
-				final ArrayList< Spot > spotsToCopy = new ArrayList<>( newspots.getNSpots( frame, false ) );
-				while ( it.hasNext() )
-					spotsToCopy.add( it.next() );
-
-				// Pass new spot list to model.
-				model.getSpots().put( frame, spotsToCopy );
-				// Make them visible
-				for ( final Spot spot : spotsToCopy )
-					spot.putFeature( SpotCollection.VISIBLITY, SpotCollection.ONE );
-
-				// Generate event for listener to reflect changes.
-				model.setSpots( model.getSpots(), true );
-
-				btnPreview.setEnabled( true );
-
-			}
-		}.start();
-	}
-
-	protected static URL getResource( final String name )
-	{
-		return StarDistDetectorFactory.class.getClassLoader().getResource( name );
 	}
 }
